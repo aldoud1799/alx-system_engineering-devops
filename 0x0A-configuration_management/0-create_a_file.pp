c156ceb640978243a7a82e5c98a09b0861cd33dc
Abdallah Abbas <abdallah.abbas.jr@gmail.com>
2023-04-21 11:30:56 -0700

    4th-0create

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # This Puppet manifest creates a file in /tmp
 
-file { 'holberton':
-  ensure  => file,
-  path    => '/tmp/holberton',
+file { '/tmp/school':
+  ensure  => 'file',
+  path    => '/tmp/school',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
